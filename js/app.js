f0a034cd2fc4140adda2b9ea6852a5a1aa46d029
Parker Couch <parkercouch@gmail.com>
2018-12-05 12:55:57 -0800

    Quote generator finished api fetch

--- js/app.js
+++ js/app.js
@@ -1,5 +1,62 @@
-$(".generate").on('click', function () {
-});
+// Backup quotes if server is slow
+const quotes = [
+    {
+        text: "So I'm just supposed to say something profound?",
+        author: "Some Person",
+    },
+    {
+        text: "Here is another one",
+        author: "Me",
+    },
+    {
+        text: "Hi!",
+        author: "Everyone",
+    },
+]
 
-$( function() {
+const fetchQuote = function () {
+    // Try to fetch random quote, if it takes 500ms then use local
+    fetchWithTimeout('https://talaikis.com/api/quotes/random/', 500)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(myJson) {
+            // console.log(myJson);
+            updateText(myJson);
+        })
+        .catch(function() {
+            // console.log('FAILED!!, Pulling local quote');
+            randomQuote(quotes);
+        });
+};
+
+
+// randomQuote :: [{quote,utterer}] -> {quote,utterer}
+const randomQuote = function (quoteList) {
+    return quoteList[Math.floor(Math.random() * quoteList.length)];
+};
+
+// updateText :: {quote, author} -> Void
+const updateText = function (quote) {
+    $('.quote-text').fadeOut(function() {
+        $(this).text(`" ${quote.quote} "`).fadeIn();
+    })
+    $('.quote-author').fadeOut(function() {
+        $(this).text(`- ${quote.author}`).fadeIn();
+    })
+};
+
+// Set timeout on quote fetch
+const fetchWithTimeout = function (url, timeout = 5000) {
+    return Promise.race([
+        fetch(url),
+        new Promise((_, reject) =>
+            setTimeout(() => reject(new Error('timeout')), timeout)
+        )
+    ]);
+}
+
+// Click event to start quote fetch!
+$(".generate").on('click', function () {
+    fetchQuote();
 });
